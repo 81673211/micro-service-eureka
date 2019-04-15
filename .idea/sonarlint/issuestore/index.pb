1434444c8a0225be7ed66372cc56651b0a4c2fe4
81673211 <81673211@qq.com>
2019-04-15 15:02:45 +0800

    eureka init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+E
+eureka-server/pom.xml,b/3/b39ae52d769ea03b699b5602439afd875433c8a7
+Š
+Zeureka-client/src/main/java/com/fred/springcloud/eurekaclient/EurekaClientApplication.java,c/b/cbf34255cab75df9214a43466e6c2421e09dca0e
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+E
+eureka-client/pom.xml,1/9/1939d6bcd0e950a81413f4056362594c66c07fe2
+…
+Ueureka-server/src/main/java/com/fred/springcloud/eureka/server/ServerApplication.java,3/9/39fed7f775d55605fc998fb7a6f1c83e0fc2076e
---
+++ eureka-client/src/main/java/com/fred/springcloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,27 @@
+package com.fred.springcloud.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private int port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam("name") String name) {
+        return String.format("hi, %s, welcome to eureka! port is %d", name, port);
+    }
+}
